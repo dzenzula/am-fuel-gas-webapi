77ca7906b04f5d06b22e4301be662c9df1d4a5eb
Dzenzura, Artem A <artem.dzenzura@arcelormittal.com>
2024-03-11 12:37:23 +0200

    Added imbalance model and renamed columns

--- models/fuelgasmodel.go
+++ models/fuelgasmodel.go
@@ -32,13 +32,36 @@ type GetDensityCoefficient struct {
 }
 
 type CalculationHistory struct {
-	StartDate       *time.Time `gorm:"column:startdate"`
+	StartDate       *time.Time `gorm:"column:start_date"`
 	CalculationDate *time.Time `gorm:"column:calculation_date"`
-	EndDate         *time.Time `gorm:"column:enddate"`
+	EndDate         *time.Time `gorm:"column:end_date"`
 	Error           string     `gorm:"column:error"`
 	UserName        string     `gorm:"column:username"`
 	SyncMode        string     `gorm:"column:syncmode"`
 	Value           *float64   `gorm:"column:dcvalue"`
+}
+
+type GetImbalanceDetails struct {
+	StartDate       *time.Time `gorm:"column:start_date"`
+	CalculationDate *time.Time `gorm:"column:calculation_date"`
+	EndDate         *time.Time `gorm:"column:end_date"`
+	ManualTotal     string     `gorm:"column:manual_total"`
+	AutoTotal       string     `gorm:"column:auto_total"`
+	AggregateTotal  string     `gorm:"column:aggregate_total"`
+	PgRedisTotal    string     `gorm:"column:pg_redis_total"`
+	Error           string     `gorm:"column:error"`
+	Username        string     `gorm:"column:username"`
+	SyncMode        string     `gorm:"column:syncmode"`
+	NodesString     string     `gorm:"column:nodes"`
+	Nodes           []Node     `gorm:"-"`
+}
+
+type Node struct {
+	Value             string `json:"value"`
+	Flag              string `json:"flag"`
+	Consumption       string `json:"consumption"`
+	GasRedistribution string `json:"gas_redistribution"`
+	Distributed       string `json:"distributed"`
 }
 
 type CalculationList struct {
